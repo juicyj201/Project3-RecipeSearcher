88794db328f7b8a5a630b88edbef774497c06da7
juicyj201 <220102473@mycput.ac.za>
2022-08-01 12:43:36 +0200

    Adding new empty pages for the team to update and work on

---
+++ app/src/main/java/za/ac/cput/recipesearcher/Activities/ProfileScreen.java
@@ -0,0 +1,16 @@
+package za.ac.cput.recipesearcher.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import za.ac.cput.recipesearcher.R;
+
+public class ProfileScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen);
+    }
+}
