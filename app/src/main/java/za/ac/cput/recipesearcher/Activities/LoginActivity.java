f0f5f4f00c26a9dc2535559b2b8b7ab54af01bf9
juicyj201 <220102473@mycput.ac.za>
2022-08-18 21:02:40 +0200

    Added changes for firebase authentication within LoginActivity and SignupActivity

--- app/src/main/java/za/ac/cput/recipesearcher/Activities/LoginActivity.java
+++ app/src/main/java/za/ac/cput/recipesearcher/Activities/LoginActivity.java
@@ -2,19 +2,29 @@ package za.ac.cput.recipesearcher.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import za.ac.cput.recipesearcher.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnSignIn;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth auth;
+    private FirebaseUser userAlreadySignedIn;
+    private FirebaseUser userSigningIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +32,65 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //setting up firebase authentication
-        mAuth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         //Initialize button
         btnSignIn = (Button) findViewById(R.id.btnSignIn);
 
-        //Button onclicks
+        //Button onclick
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //login user from google firebase previous json records
-
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                final String email = findViewById(R.id.edtEmailAddress).toString();
+                final String password = findViewById(R.id.editTextTextPassword).toString();
+                
+                if(!email.equals(null) && !password.equals(null)){
+                    //validate and sign in the user with google verification
+                    signIn(email, password);
+                }
             }
         });
+    }
+
+    private void signIn(String email, String password){
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Log.d(TAG, "signInWithEmail:success");
+                            userSigningIn = auth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        } else {
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            
+                            //TODO error activity or toast will be implementted
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //get the user that is currently logging in
+        userAlreadySignedIn = auth.getCurrentUser();
+
+        //validate user
+        if(!userAlreadySignedIn.equals(null)) {
+            updateUserInfo(userAlreadySignedIn);
+        }
+    }
+
+    private void updateUserInfo(FirebaseUser userAlreadySignedIn){
+        final TextView emailUpdateText = (TextView) findViewById(R.id.edtEmailAddress);
+        emailUpdateText.setText(userAlreadySignedIn.getEmail());
+        emailUpdateText.findFocus();
+        //TODO Cant seem to get this working properly - i will create a hotfix for this after sign in and signup attempts work
+        //final TextView passwordUpdateText = (TextView) findViewById(R.id.editTextTextPassword);
+        //passwordUpdateText.setText(user.getPassword());
     }
 }
