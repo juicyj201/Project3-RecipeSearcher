da4c8a3707c1681e0bd4270b2811fd4aa30354a0
juicyj201 <220102473@mycput.ac.za>
2022-10-21 18:13:41 +0200

    Added tests for recipe crud methods

--- app/src/main/java/za/ac/cput/recipesearcher/Repository/Impl/RecipeRepositoryImpl.java
+++ app/src/main/java/za/ac/cput/recipesearcher/Repository/Impl/RecipeRepositoryImpl.java
@@ -6,66 +6,62 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import za.ac.cput.recipesearcher.Entities.RVSubCategoryModel;
 import za.ac.cput.recipesearcher.Repository.RecipeRepository;
 
 public class RecipeRepositoryImpl implements RecipeRepository {
-    private FirebaseDatabase db;
-    private DatabaseReference reciperepo;
+    private DatabaseReference ref = FirebaseDatabase.getInstance().getReference("https://recipesearcher-6b2ab-default-rtdb.firebaseio.com");
+    private ValueEventListener listener;
 
     public RecipeRepositoryImpl(){
-        db = FirebaseDatabase.getInstance();
-        reciperepo = db.getReference("recipe");
     }
 
     @Override
     public RVSubCategoryModel save(RVSubCategoryModel recipe) {
         if(!recipe.equals(null)){
-            reciperepo.child(recipe.getRecipeName()).setValue(recipe);
+            //ref.child("recipe").child(recipe.getRecipeName()).setValue(recipe);
+            ref.child("recipe").child(recipe.getRecipeName()).setValue(recipe);
+            return ref.child("recipe").child(recipe.getRecipeName()).get().getResult().getValue(RVSubCategoryModel.class);
         }
 
-        return recipe;
+        return null;
     }
 
     @Override
     public RVSubCategoryModel read(RVSubCategoryModel recipe) {
-        RVSubCategoryModel dbRecipe;
-
-        //if there is a recipe with the specific searched for, return task object using onComplete method
-        Task<DataSnapshot> snap = reciperepo.child(recipe.getRecipeName()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>(){
+        return ref.child("recipe").child(recipe.getRecipeName()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isComplete()){
-                    Log.i("RecipeRepositoryImpl", String.valueOf(task.getResult().getValue(RVSubCategoryModel.class)));
-                }
+                Log.i("RecipeRepositoryImpl", String.valueOf(task.getResult().getValue(RVSubCategoryModel.class)));
             }
-        });
-
-        return snap.getResult().getValue(RVSubCategoryModel.class);
+        }).getResult().getValue(RVSubCategoryModel.class);
     }
 
     @Override
     public List<RVSubCategoryModel> readAll() {
         List<RVSubCategoryModel> recipeList = new ArrayList<>();
 
-        Task<DataSnapshot> snap = reciperepo.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>(){
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isComplete()){
+        while (ref.child("recipe").get().getResult().exists()) {
+            RVSubCategoryModel mod = ref.child("recipe").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
                     Log.i("RecipeRepositoryImpl", String.valueOf(task.getResult().getValue(RVSubCategoryModel.class)));
                 }
-            }
-        });
+            }).getResult().getValue(RVSubCategoryModel.class);
 
-        while(snap.isSuccessful()){
-            recipeList.add(snap.getResult().getValue(RVSubCategoryModel.class));
+            recipeList.add(mod);
         }
 
         return recipeList;
